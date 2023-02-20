eb59061920ea7f1e1a18f0c6b484ef3511184ce4
Sapereza <s.perezanderson@gmail.com>
2023-02-19 22:25:10 -0500

    testing design

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
-function menu() {
-    var x = document.getElementById("navList");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
+//function menu() {
+   // var x = document.getElementById("navList");
+   // if (x.style.display === "block") {
+   //   x.style.display = "none";
+   // } else {
+   //   x.style.display = "block";
+  //  }
+  //}
+
+  const navList = document.querySelector('.navList')
+  function onToggleMenu(e){
+    e.name = e.name === 'menu' ? 'menu' : 'menu;'
+    navList.classList.toggle('top-[9%]');
   }
