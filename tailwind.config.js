c8a977a090596cd7e7b7f54414589fb27d979096
Sapereza <s.perezanderson@gmail.com>
2023-02-19 19:20:41 -0500

    created main page files/ added the begining stages of the navigation

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}","./index.html","./aboutMe.html","./gallery.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
